f135ee8c5766652eeb98e81ae957a4399d28bf7e
Tmiracle <1720570275@qq.com>
2020-02-17 23:01:00 +0800

    1.0.3 新增页面

--- statics/js/common.js
+++ statics/js/common.js
@@ -2,8 +2,7 @@ $(function () {
     // pjax异步加载页面
     $(document).pjax('a', '.pjax-container', {
         fragment: '.pjax-container',
-        timeout: 5000,
-        async: false
+        timeout: 5000
     }).on('pjax:complete', function () {
 
     });
