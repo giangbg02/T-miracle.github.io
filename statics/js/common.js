01865ab2600f2f4794c5ab96fc2b67ae3b58e8aa
Tmiracle <1720570275@qq.com>
2020-02-22 00:44:59 +0800

    1.0.11 新增所有文章页，更新头像CSS

--- statics/js/common.js
+++ statics/js/common.js
@@ -1,50 +1,45 @@
 $(function () {
     GenerateContentList();
-
-    // pjax异步加载页面
-    $(document).pjax('a', '.pjax-container', {
-        fragment: '.pjax-container',
-        timeout: 5000
-    }).on('pjax:complete', function () {
-        GenerateContentList();
-    });
-
-    //监听后退按钮触发事件，重载js资源
-    window.onpopstate = function () {
-        GenerateContentList();
-    };
+    GenerateToc();
 });
 
+//pjax异步加载页面
+$(document).pjax('a', '.pjax-container', {
+    fragment: '.pjax-container',
+    scrollTo: 0,
+    timeout: 5000
+});
+
+//pjax在页面加载完成后执行
+$(document).on('pjax:complete', function () {
+    GenerateContentList();
+    GenerateToc();
+});
+
+//在页面执行前进后退事件后执行
+/*$(document).on('pjax:popstate', function () {
+    GenerateContentList();
+    GenerateToc();
+});*/
+window.onpopstate = function () {
+    GenerateContentList();
+    GenerateToc();
+};
+
 function GenerateContentList() {
-    //弹出侧边目录
+//弹出侧边目录
     $(".aside-btn").on("click", function () {
         $(".aside-btn").toggleClass("active");
         $("main").toggleClass("active");
         $("aside").toggleClass("active");
     });
 
-    //判断页面是否含有文章目录容器，如果有，则加载文章目录
-    if ($("#toc").length > 0) {
-        $('#toc').empty();
-        $("#toc").tocify({
-            context: 'main',
-            selectors: 'h2,h3,h4',
-            scrollTo: 60,
-            showAndHide: true,
-            smoothScroll: true,
-            history: false
-        }).data("toc-tocify");
-
-        prettyPrint();
-        $(".optionName").popover({trigger: "hover"});
-    }
-
-    //判断页面是否含有代码块，如果有，则Prism渲染代码块
+//判断页面是否含有代码块，如果有，则Prism渲染代码块
     if ($('code').length > 0) {
         Prism.highlightAll();
     }
 
-    // 判断页面是否含有gitalk容器，如果有，则加载gitalk评论系统
+// 判断页面是否含有gitalk容器，如果有，则加载gitalk评论系统
     if ($('#gitalk-container').length > 0) {
         var gitalk = new Gitalk({
             clientID: '7ff38d8c3509efdcee12',   // GitHub Application Client ID
@@ -57,7 +52,7 @@ function GenerateContentList() {
         gitalk.render('gitalk-container');      // 渲染Gitalk评论组件
     }
 
-    // 未完成文章点击提示信息
+// 未完成文章点击提示信息
     $('.tip').click(function () {
         top.layer.msg(
             '<svg style="width: 36px;height: 36px" class="icon" aria-hidden="true"><use xlink:href="#icon-biji"></use></svg>文章正在努力编写中...',
@@ -67,4 +62,23 @@ function GenerateContentList() {
                 btn: ['明白了']
             })
     });
+
+//外链网址设置新标签打开
+    $('a[href*="http"]').attr("target", "_blank");
+}
+
+function GenerateToc() {
+//判断页面是否含有文章目录容器，如果有，则加载文章目录
+    var toc = $("#toc");
+    toc.empty();
+    if (toc.length > 0) {
+        toc.tocify({
+            context: 'main',
+            selectors: 'h2,h3,h4',
+            scrollTo: 60,
+            showAndHide: true,
+            smoothScroll: true,
+            history: false
+        }).data("toc-tocify");
+    }
 }
